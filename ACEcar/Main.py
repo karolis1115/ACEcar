964a106c7120752531d082fdc24ca4decdbbf81c
karolis1115 <karolisjuoz@gmail.com>
2022-05-17 14:10:16 +0200

    Added keyboard control code to main code and tested on prototype

--- ACEcar/Main.py
+++ ACEcar/Main.py
@@ -1,6 +1,17 @@
 import cv2
+import pigpio
+import keyboard
 import numpy as np
 #print(cv2.getBuildInformation())
+
+
+steerPWM =13
+drivePWM =12
+driveDirrection= 5
+
+pi = pigpio.pi('acecar.local')
+pi.set_mode(driveDirrection, pigpio.OUTPUT)
+pi.set_mode(drivePWM, pigpio.OUTPUT)
 
 ########################MAIN##############################
 
@@ -10,6 +21,27 @@ cap = cv2.VideoCapture('http://acecar.local:8080/?action=stream')
 
 #keep updating the frame FoReVeR
 while (True):
+    if keyboard.is_pressed('w'):
+        pi.set_PWM_dutycycle(driveDirrection,128)
+        pi.write(drivePWM,0)
+   
+    elif keyboard.is_pressed('s'):
+        pi.write(driveDirrection,0)
+        pi.set_PWM_dutycycle(drivePWM,128)
+
+
+    elif keyboard.is_pressed('a'):
+        pi.set_servo_pulsewidth(steerPWM, 1200)
+
+   
+    elif keyboard.is_pressed('d'):
+        pi.set_servo_pulsewidth(steerPWM, 1700)
+
+    else:
+        pi.set_servo_pulsewidth(steerPWM, 1450)
+        pi.write(driveDirrection,0)
+        pi.write(drivePWM,0)
+
     Ret,frame = cap.read()
     if not Ret:
         print("no valid frame")
