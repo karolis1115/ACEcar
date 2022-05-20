3954ed1b5d05a6e6402857f19f29f8e20addce94
Karolis <karolisjuoz@gmail.com>
2022-05-20 21:55:14 +0200

    Merge branch 'Karolis's-branch'

--- ACEcar/ProtoMain.py
+++ ACEcar/ProtoMain.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import cv2
 import pigpio
 import keyboard
@@ -5,8 +6,6 @@ import threading
 import time
 import numpy as np
 # print(cv2.getBuildInformation())
-
-
 steerPWM = 13
 drivePWM = 12
 driveDirrection = 5
@@ -15,19 +14,16 @@ pi = pigpio.pi('acecar.local')
 pi.set_mode(driveDirrection, pigpio.OUTPUT)
 pi.set_mode(drivePWM, pigpio.OUTPUT)
 
-########################MAIN##############################
 
-'''
-Take video data -> detect line + obstacles + end -> follow line + avoid obstacles + stop at end
-'''
+########################MAIN##############################
 
 
 def video():
     cap = cv2.VideoCapture('http://acecar.local:8080/?action=stream')
     while True:
         # Opens capture source which is simply a url to the video stream
-        Ret, frame = cap.read()
-        if not Ret:
+        frame = cap.read()
+        if not frame:
             print("no valid frame")
             break
         # show the video output
@@ -41,82 +37,89 @@ def video():
 def control():
     while True:
         if keyboard.is_pressed('w'):
-            pi.set_PWM_dutycycle(driveDirrection, 200)
+            pi.set_PWM_dutycycle(driveDirrection, 120)
             pi.write(drivePWM, 0)
 
         elif keyboard.is_pressed('s'):
             pi.write(driveDirrection, 0)
-            pi.set_PWM_dutycycle(drivePWM, 200)
+            pi.set_PWM_dutycycle(drivePWM, 120)
 
         elif keyboard.is_pressed('a'):
-            pi.set_servo_pulsewidth(steerPWM, 1200)
+            pi.set_servo_pulsewidth(steerPWM, 1150)
 
         elif keyboard.is_pressed('d'):
-            pi.set_servo_pulsewidth(steerPWM, 1700)
+            pi.set_servo_pulsewidth(steerPWM, 1550)
 
         else:
-            pi.set_servo_pulsewidth(steerPWM, 1450)
+            #pi.set_servo_pulsewidth(steerPWM, 1350)
             pi.write(driveDirrection, 0)
             pi.write(drivePWM, 0)
+            #pi.set_servo_pulsewidth(steerPWM, 0)
 
 
 def main():
+    global trg
+    trg = 0
 
-    #cap = cv2.VideoCapture('vid1.mp4')
     cap = cv2.VideoCapture('http://acecar.local:8080/?action=stream')  # For live video
-    cap.set(3, 160)
-    cap.set(4, 120)
+    #cap.set(3, 160)
+    #cap.set(4, 120)
+
+    # Drive forward
+    pi.set_PWM_dutycycle(driveDirrection, 100) # int is power (from 0-255)
+    pi.write(drivePWM, 0) #when dirrection is set drive power is reversed 
+
+    low_b = np.uint8([255, 255, 255])  # color of background
+    high_b = np.uint8([80, 80, 150])
 
     while True:
-        # Drive forward
-        pi.set_PWM_dutycycle(driveDirrection, 150) # int is power (from 0-255)
-        pi.write(drivePWM, 0) #when dirrection is set drive power is reversed 
-
-        ret, frame = cap.read()
-
-        low_b = np.uint8([255, 255, 255])  # color of background
-
-        high_b = np.uint8([80, 80, 150])
+        Ret, frame = cap.read()
         mask = cv2.inRange(frame, high_b, low_b)
-
-        contours, hierarchy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
+        contours, hierachy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
             M = cv2.moments(c)
             if M["m00"] != 0:
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
-                print("CX: " + str(cx) + " CY:" + str(cy))
-                if cx >= 1200:
-                    print("Turn Left")
-                    # turn wheels left (probs needs adjusting)
-                    pi.set_servo_pulsewidth(steerPWM, 1200)
-
-                if cx < 1200 and cx > 1000:  # NEED TO CHANGE VALUES DEPENDS OF CAMERA
-                    print("On track")
-                    # center wheels (probs needs adjusting)
-                    pi.set_servo_pulsewidth(steerPWM, 1450)
-
-                if cx <= 1100:
-                    print("Turn Right")
-                    # turn wheels right (probs needs adjusting)
-                    pi.set_servo_pulsewidth(steerPWM, 1700)
+                trg = cx
+                #print("CX: " + str(cx) + " CY:" + str(cy))
+                
                 cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-
         cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
-        cv2.imshow("Mask", mask)
-        cv2.imshow("Grame", frame)
+        #cv2.imshow("Mask", mask)
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xff == ord('q'):  # 1 is the time in ms
+            cap.release()
+            cv2.destroyAllWindows()
+            pi.set_PWM_dutycycle(driveDirrection, 0)
+            pi.set_servo_pulsewidth(steerPWM, 1350)
             break
 
-    cap.release()
-    cv2.destroyAllWindows()
+
+def camride():
+    while True:
+        print(trg)
+        if trg <= 340:
+            #print("Turn Left")
+            pi.set_servo_pulsewidth(steerPWM, 1150)
+
+        if trg < 340 and trg > 300:
+            #print("On track")
+            pi.set_servo_pulsewidth(steerPWM, 1350)
+
+        if trg >= 300:
+            #print("Turn Right")
+            pi.set_servo_pulsewidth(steerPWM, 1550)
 
 
 tmain = threading.Thread(target=main)
 tmain.start()
+
+tcamride = threading.Thread(target=camride)
+tcamride.start()
+
 #tcontrol = threading.Thread(target=control)
+#tcontrol.start()
 #tvideo = threading.Thread(target=video)
 #tvideo.start()
-#tcontrol.start()
